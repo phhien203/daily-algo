613735881bbc9aae436cb2549e7abb3ca7e38eac
Pham Huu Hien <phhien203@gmail.com>
2022-07-03 13:20:31 +0700

    Implement async/await handle promise reject in the middle

--- JS/implement-async-await.v2.ts
+++ JS/implement-async-await.v2.ts
@@ -9,13 +9,30 @@ function addAsync(x: number, y: number): Promise<number> {
   });
 }
 
+function getDataAsync(url: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 300);
+  });
+}
+
 function asyncRunner(g: () => Generator): void {
   console.log("generator initialization");
   const it = g();
   let iterationCounter = 0;
+  let errorFlag = false;
 
   function iterate(value: unknown): void {
-    const yielded = it.next(value);
+    let yielded: IteratorResult<unknown>;
+
+    if (errorFlag) {
+      yielded = it.throw(value);
+      errorFlag = false;
+    } else {
+      yielded = it.next(value);
+    }
+
     console.log("iteration start", iterationCounter);
     iterationCounter++;
     const startTime = Date.now();
@@ -36,7 +53,8 @@ function asyncRunner(g: () => Generator): void {
       .catch((err) => {
         const elapsedTime = Date.now() - startTime;
         console.log(`promise resolved ${elapsedTime}ms`);
-        it.throw(err);
+        errorFlag = true;
+        iterate(err);
       });
 
     console.log("runner: attached then to promise");
@@ -49,7 +67,7 @@ function* main(): any {
   try {
     const result = yield addAsync(2, 3);
     console.log("main", result);
-    const result2 = yield addAsync(10, -1);
+    const result2 = yield getDataAsync('twitter.com');
     console.log("main2", result2);
   } catch (err) {
     console.log("error handling in main");
