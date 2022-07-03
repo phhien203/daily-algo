84d018b55031ad413ae82ddea4f7ab7fe82f79d8
Pham Huu Hien <phhien203@gmail.com>
2022-07-03 13:14:21 +0700

    Implement asyn/await support multiple async call

--- JS/implement-async-await.v2.ts
+++ JS/implement-async-await.v2.ts
@@ -1,9 +1,10 @@
 function addAsync(x: number, y: number): Promise<number> {
   console.log("timeout started");
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("timeout finished");
       resolve(x + y);
+      // reject();
     }, 500);
   });
 }
@@ -11,21 +12,48 @@ function addAsync(x: number, y: number): Promise<number> {
 function asyncRunner(g: () => Generator): void {
   console.log("generator initialization");
   const it = g();
+  let iterationCounter = 0;
 
-  const startTime = Date.now();
-  const promise: Promise<unknown> = it.next().value;
-  promise.then((value) => {
-    const elapsedTime = Date.now() - startTime;
-    console.log(`promise resolved ${elapsedTime}ms`);
-    it.next(value);
-  });
+  function iterate(value: unknown): void {
+    const yielded = it.next(value);
+    console.log("iteration start", iterationCounter);
+    iterationCounter++;
+    const startTime = Date.now();
 
-  console.log("runner: attached then to promise");
+    if (yielded.done) {
+      console.log("generator ended");
+      return;
+    }
+
+    const promise: Promise<unknown> = Promise.resolve(yielded.value);
+
+    promise
+      .then((value) => {
+        const elapsedTime = Date.now() - startTime;
+        console.log(`promise resolved ${elapsedTime}ms`);
+        iterate(value);
+      })
+      .catch((err) => {
+        const elapsedTime = Date.now() - startTime;
+        console.log(`promise resolved ${elapsedTime}ms`);
+        it.throw(err);
+      });
+
+    console.log("runner: attached then to promise");
+  }
+
+  iterate(undefined);
 }
 
 function* main(): any {
-  const result = yield addAsync(2, 3);
-  console.log("main", result);
+  try {
+    const result = yield addAsync(2, 3);
+    console.log("main", result);
+    const result2 = yield addAsync(10, -1);
+    console.log("main2", result2);
+  } catch (err) {
+    console.log("error handling in main");
+  }
 }
 
 asyncRunner(main);
