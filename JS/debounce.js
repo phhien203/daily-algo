44a1c03be05e6c5d345b12d41184f6107aa68df6
Pham Huu Hien <phhien203@gmail.com>
2022-07-07 08:31:23 +0700

    Implement debounce function

---
+++ JS/debounce.js
@@ -0,0 +1,18 @@
+function debounce(callback, delay, immediate = false) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    if (immediate && timerId == null) {
+      callback.apply(this, args);
+    }
+
+    timerId = setTimeout(() => {
+      if (!immediate) {
+        callback.apply(this, args);
+      }
+      timerId = null;
+    }, delay);
+  };
+}
